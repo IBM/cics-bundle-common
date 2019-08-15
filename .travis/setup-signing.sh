f047d225bc4038658e0dea55d378f40f1659e94c
Ben Cox <1038350+ind1go@users.noreply.github.com>
2019-08-15 12:26:27 +0100

    Add encrypted variables for signing and deployment
    Available variables:
    - OSSRH_USERNAME
    - OSSRH_PASSWORD
    - GPG_KEY_NAME
    - GPG_PASSPHRASE
    
    Signed-off-by: Ben Cox <1038350+ind1go@users.noreply.github.com>
    Signed-off-by: Sophie Green <SOPHIEGR@uk.ibm.com>

--- .travis/setup-signing.sh
+++ .travis/setup-signing.sh
@@ -3,7 +3,7 @@ set -x
 
 # If this is a master build then lay out signing information
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-  openssl aes-256-cbc -K $encrypted_1e42ed2d98cb_key -iv $encrypted_1e42ed2d98cb_iv -in .travis/signingkey.asc.enc -out .travis/signingkey.asc -d
+  openssl aes-256-cbc -K $encrypted_6b1cbeda7dc5_key -iv $encrypted_6b1cbeda7dc5_iv -in .travis/signingkey.asc.enc -out .travis/signingkey.asc -d
   gpg --version
   gpg --fast-import .travis/signingkey.asc
   rm .travis/signingkey.asc*
