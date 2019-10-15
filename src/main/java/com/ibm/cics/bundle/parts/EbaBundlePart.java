93fef9b39e3282ed525433369c65dbb074a6af7e
Dave Nice <dave.nice@uk.ibm.com>
2019-10-15 13:34:36 +0100

    add EBA support
    Signed-off-by: Dave Nice <dave.nice@uk.ibm.com>

---
+++ src/main/java/com/ibm/cics/bundle/parts/EbaBundlePart.java
@@ -0,0 +1,32 @@
+package com.ibm.cics.bundle.parts;
+
+/*-
+ * #%L
+ * CICS Bundle Common Parent
+ * %%
+ * Copyright (C) 2019 IBM Corp.
+ * %%
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * #L%
+ */
+
+import java.io.File;
+
+public class EbaBundlePart extends AbstractJavaBundlePart  {
+	
+	public EbaBundlePart(String symbolicName, String jvmServer, File warFile) {
+		super(
+			symbolicName,
+			BundlePartType.EBABUNDLE,
+			symbolicName,
+			jvmServer,
+			warFile,
+			"eba"
+		);
+	}
+
+}
