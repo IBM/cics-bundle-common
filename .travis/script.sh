6cabdc766e1ce937dd45220be722b52defe1325a
Steve Bolton <boltonst@uk.ibm.com>
2020-08-06 16:10:11 +0100

    One more fix!
    Signed-off-by: Steve Bolton <boltonst@uk.ibm.com>

--- .travis/script.sh
+++ .travis/script.sh
@@ -3,7 +3,7 @@ set -x
 
 # If this is a master build then deploy at the end
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-  mvn -Dstyle.color=always -B -U -P sign --settings travis-settings.xml verify spotbugs:spotbugs spotbugs:check deploy;
+  mvn -Dstyle.color=always -B -U -P sign --settings travis-settings.xml verify deploy;
 # Otherwise just build and test
 else
   mvn -Dstyle.color=always -B -U --settings travis-settings.xml verify;
