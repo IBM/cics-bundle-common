6a1b78912edf7a00a51107008f033c47bf2d2f08
Kye Maloy <kye.maloy@ibm.com>
2023-01-17 15:55:55 +0000

    fix: linter issues
    Signed-off-by: Kye Maloy <kye.maloy@ibm.com>

--- src/main/java/com/ibm/cics/bundle/parts/AbstractJavaBundlePart.java
+++ src/main/java/com/ibm/cics/bundle/parts/AbstractJavaBundlePart.java
@@ -4,7 +4,7 @@ package com.ibm.cics.bundle.parts;
  * #%L
  * CICS Bundle Common Parent
  * %%
- * Copyright (C) 2019 IBM Corp.
+ * Copyright (C) 2019, 2023 IBM Corp.
  * %%
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -23,6 +23,7 @@ import java.io.InputStream;
 import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.List;
+
 import javax.xml.transform.TransformerException;
 
 import org.w3c.dom.Document;
